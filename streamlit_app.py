a2be2fc1598ff171e887b12c1ef67118130f7257
Ahmed Ramadan <104329133+AhmedRamadana@users.noreply.github.com>
2024-08-28 03:22:45 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 st.title("🎈 My new app")
 st.number_input('input1', min_value= 0, max_value=9, value=1 )
+st.number_input('input2', min_value= 0, max_value=9, value=1 )
+st.number_input('input3', min_value= 0, max_value=9, value=1 )
