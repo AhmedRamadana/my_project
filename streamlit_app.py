5b85e9ac592cf9e578f1286096ec75102c774f40
Ahmed Ramadan <104329133+AhmedRamadana@users.noreply.github.com>
2024-08-28 03:26:30 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
-
 st.title("🎈 My new app")
-st.number_input('input1', min_value= 0, max_value=9, value=1 )
-st.number_input('input2', min_value= 0, max_value=9, value=1 )
-st.number_input('input3', min_value= 0, max_value=9, value=1 )
+
+input1= st.number_input('input1', min_value= 0, max_value=9, value=1 )
+input2= st.number_input('input2', min_value= 0, max_value=9, value=1 )
+input3= st.number_input('input3', min_value= 0, max_value=9, value=1 )
+
+output = model.predict([[input1,input2,input3]])
+
+st.write("output",output)
